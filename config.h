4fa34d5a28041c5e2dab58d174cf84eca1636e19
Mike Smith <mikejamesjsmith@gmail.com>
2020-11-01 20:58:12 +0000

    new stuff

--- config.h
+++ config.h
@@ -18,7 +18,7 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int extrabar           = 1;        /* 0 means no extra bar */
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "f1", "f2", "f3", "f4" };/*, "f5", "f6", "f7", "f8", "f9" };*/
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -65,7 +65,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_b,      toggleextrabar, {0} },
+	{ MODKEY|ShiftMask,             XK_b,      toggleextrabar, {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -101,11 +101,11 @@ static Key keys[] = {
 	TAGKEYS(                        XK_F2,                     10)
 	TAGKEYS(                        XK_F3,                     11)
 	TAGKEYS(                        XK_F4,                     12)
-	TAGKEYS(                        XK_F5,                     13)
+      /*TAGKEYS(                        XK_F5,                     13)
 	TAGKEYS(                        XK_F6,                     14)
 	TAGKEYS(                        XK_F7,                     15)
 	TAGKEYS(                        XK_F8,                     16)
-	TAGKEYS(                        XK_F9,                     17)
+	TAGKEYS(                        XK_F9,                     17)*/
 	{ MODKEY|ShiftMask,             XK_c,      quit,           {0} },
 };
 
